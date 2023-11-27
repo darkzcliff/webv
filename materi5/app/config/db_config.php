6d25b5ca88474196068402a8fcffd1d3cc39b1bb
piko <51285796+darkzcliff@users.noreply.github.com>
2023-11-27 20:20:37 +0700

    Add files via upload

---
+++ materi5/app/config/db_config.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\config;
+
+class db_config {
+    public $host = "localhost";
+    public $user = "root";
+    public $passwd = "";
+    public $db_name = "db_web";
+    public $port = 3306;
+}
+
+?>
